d930b1526f8365955f8345a4c8ac89eb51d0e023
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2024-03-07 20:06:06 +1100

    Wow

--- nozl.js
+++ nozl.js
@@ -1,12 +1,131 @@
 var table = document.getElementById("table-body");
 var weapons = [];
 var points = [];
+var level = [];
+var maxPoints = [];
 var max;
-
+var current = 0;
+var remaining = 3;
+var background;
 function loadValues() {
     for (i=0; i<table.rows.length; i++) {
         weapons[i] = table.rows[i].cells[0].innerHTML;
         points[i] = table.rows[i].cells[1].innerHTML;
+        level[i] = 1;
     }
-    max = table.rows[1].cells[8].innerHTML;
+    maxPoints[0] = table.rows[1].cells[8].innerHTML;
+    maxPoints[1] = table.rows[2].cells[8].innerHTML;
+    maxPoints[2] = table.rows[3].cells[8].innerHTML;
+    max = maxPoints[0];
+    createTables();
+}
+
+function createTables() {
+    var check = 0;
+    table = document.getElementById("comp")
+    table.innerHTML = "<thead> <tr style='background-color:#34a853; font-size=20px'> <th></th> <th>Weapons</th> <th>Points</th> </tr> </thead> <tbody id='table-body'>";
+    for (i=0; i<weapons.length; i++) {
+        if (level[i] == 0) {
+            if (check % 2 == 0) {
+                background = 1;
+            } else {
+                background = 2;
+            }
+            check++;
+            table.innerHTML += "<tr id='c" + i + "' class='tableText" + background + "' onclick='outComp(id)'> <td> <img src='img/" + (weapons[i].replace(".", "")).replace("'", "") + ".png' width='60px' height='60px'> </td> <td>" + weapons[i] + "</td> <td>" + points[i] + "</td> </tr>"
+        }
+        }
+    table.innerHTML += "</tbody>";
+    check = 0;
+
+    table = document.getElementById("table-body")
+    table.innerHTML = "<thead> <tr style='background-color:#34a853; font-size=20px'> <th></th> <th>Weapons</th> <th>Points</th> </tr> </thead> <tbody id='table-body'>";
+    for (i=0; i<weapons.length; i++) {
+        if (level[i] == 1) {
+            if (check % 2 == 0) {
+                background = 1;
+            } else {
+                background = 2;
+            }
+            check++;
+            table.innerHTML += "<tr id='a" + i + "' class='tableText" + background + "' onclick='toComp(id)'> <td> <img src='img/" + (weapons[i].replace(".", "")).replace("'", "") + ".png' width='60px' height='60px'> </td> <td>" + weapons[i] + "</td> <td>" + points[i] + "</td> </tr>"
+        }
+        }
+    table.innerHTML += "</tbody>";
+    check = 0;
+
+    table = document.getElementById("full")
+    table.innerHTML = "<thead> <tr style='background-color:color-mix(in srgb,#34a853, black 50%); font-size=20px'> <th></th> <th>Weapons</th> <th>Points</th> </tr> </thead> <tbody id='table-body'>";
+    for (i=0; i<weapons.length; i++) {
+        if (level[i] == 2) {
+            if (check % 2 == 0) {
+                background = 3;
+            } else {
+                background = 4;
+            }
+            check++;
+            table.innerHTML += "<tr id='u" + i + "' class='tableText" + background + "'> <td> <img src='img/" + (weapons[i].replace(".", "")).replace("'", "") + ".png' width='60px' height='60px'> </td> <td>" + weapons[i] + "</td> <td>" + points[i] + "</td> </tr>"
+        }
+        }
+    table.innerHTML += "</tbody>";
+}
+
+function toComp(id) {
+    id = id.substring(1);
+    level[id] = 0;
+    current += Number(points[id]);
+    remaining--;
+    checkComp();
+}
+
+function outComp(id) {
+    id = id.substring(1);
+    level[id] = 1;
+    current -= Number(points[id]);
+    remaining++;
+    checkComp();
+}
+
+function checkComp() {
+    for (i=0; i<weapons.length; i++) {
+        if (level[i] == 1) {
+            if (points[i] > max - current - 2 * remaining || current == max) {
+                level[i] = 2;
+            }
+        } else if (level[i] == 2) {
+            if (points[i] <= max - current - 2 * remaining) {
+                level[i] = 1;
+            }
+        }
+    }
+    createTables();
+}
+
+function showMenu() {
+    document.getElementById("menu").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.menuButton')) {
+      var dropdowns = document.getElementsByClassName("menu");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+
+function changeMax(id) {
+    max = maxPoints[id];
+    document.getElementById("menuButton").innerHTML = document.getElementById(id).innerHTML;
+    remaining = Number(document.getElementById(id).innerHTML) - 1;
+    for (i=0; i<weapons.length; i++) {
+        if (level[i] != 1) {
+            level[i] = 1;
+        }
+    }
+    createTables();
 }
